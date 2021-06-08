c372b89e94ce0797e740f04ee7e3a1e8b7fb68c9
AlexanderSolodyankin <89515014507@mail.ru>
2021-06-08 21:07:15 +0600

    Lessons 30

---
+++ src/Lessons/lessons30/Cats.java
@@ -0,0 +1,24 @@
+package Lessons.lessons30;
+
+ class Cats {
+    private String name;
+
+     public Cats(String name) {
+         this.name = name;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     @Override
+     public String toString() {
+         return "Cats{" +
+                 "name='" + name + '\'' +
+                 '}';
+     }
+ }
---
+++ src/Lessons/lessons30/lessons30.java
@@ -0,0 +1,27 @@
+package Lessons.lessons30;
+
+ class lessons30 {
+    public static void main(String[] args) {
+
+//
+//       Cats[] cats = new Cats[4];
+//       for(int i = 0; i< cats.length; i++){
+//
+//           if(i == (cats.length - 1)){
+//               cats[i] = new Cats(null);
+//           }else {
+//               cats[i] = new Cats("Catser " + i );
+//           }
+//       }
+//
+//       try {
+//
+//               for (Cats itwm : cats) {
+//                   System.out.println(itwm.getName());
+//               }
+//           }catch (NullPointerException e){
+//           System.out.println("Пустое значение в элементе масива");
+//       }
+
+    }
+}
